0b1085db6003f3cea038f66c4b645a1f51ec204c
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-06 13:40:31 +0100

    initialize project structure and main classes

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,5 +1,8 @@
 package se.lexicon;
 
+import se.lexicon.model.*;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,44 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Product soda = new Soda(23, "Coca Cola");
+        Product chips = new Chips(30, "Potato");
+        Product chocolate= new Chocolate(15, "Mars");
+
+        Product[] Products = new Product[]{soda, chips, chocolate};
+
+
+        VendingMachineImpl vendingMachine = new VendingMachineImpl(Products);
+
+        System.out.println(Arrays.toString(vendingMachine.getProducts()));
+
+        System.out.println(vendingMachine.getDescription(0));
+
+        vendingMachine.addCurrency(Currency.FIVE);
+        vendingMachine.addCurrency(Currency.TEN);
+        vendingMachine.addCurrency(Currency.TEN);
+        vendingMachine.addCurrency(Currency.TWO);
+        vendingMachine.addCurrency(Currency.TWO);
+
+        System.out.println(vendingMachine.getBalance());
+
+        try {
+            Product requested = vendingMachine.request(1);
+            System.out.println("You bought: " + requested.getProductName());
+
+        }catch (RuntimeException e) {
+            System.out.println("Too expensive");
+        }
+
+        try {
+            vendingMachine.request(2);
+        }catch (RuntimeException e) {
+            System.out.println("Too expensive");
+        }
+        System.out.println(vendingMachine.getBalance());
+
+        vendingMachine.endSession();
+
+
     }
 }
