52b59478fb68fc5c7a17824de251c28074b05456
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-06 13:41:24 +0100

    implement Soda, Chips, and Chocolate classes

---
+++ src/main/java/se/lexicon/model/Chips.java
@@ -0,0 +1,33 @@
+package se.lexicon.model;
+
+public class Chips implements Product {
+    private final int id;
+    private double price;
+    private String productName;
+
+    public Chips(double price, String productName) {
+        this.id = Product.SEQUENCER.getAndIncrement();
+        this.price = price;
+        this.productName = productName;
+    }
+
+    @Override
+    public String examine() {
+        return "Chips{" +
+                "id=" + id +
+                ", price=" + price +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
+
+    @Override
+    public String use() {
+        return "You eat the chips.";
+    }
+
+    @Override public int getId() { return id; }
+    @Override public double getPrice() { return price; }
+    @Override public void setPrice(double price) { this.price = price; }
+    @Override public String getProductName() { return productName; }
+    @Override public void setProductName(String productName) { this.productName = productName; }
+}
