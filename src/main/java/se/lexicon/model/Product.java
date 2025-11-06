da142210b3c5bec283eeac90bc488d8c15ce0882
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-06 13:40:50 +0100

    add Product interface with sequencer and base methods

--- src/main/java/se/lexicon/model/Product.java
+++ src/main/java/se/lexicon/model/Product.java
@@ -1,11 +1,14 @@
 package se.lexicon.model;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public interface Product {
-  String examine();
-  String use();
-  int getid();
-  String getProductName();
-  void setProductName();
-  double getPrice();
-  void setPrice(double price);
+    AtomicInteger SEQUENCER = new AtomicInteger(0);
+    String examine();
+    String use();
+    int getId();
+    String getProductName();
+    void setProductName(String productName);
+    double getPrice();
+    void setPrice(double price);
 }
